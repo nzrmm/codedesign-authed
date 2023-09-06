0b5c5f20670008bba3fe8d8a7c400ace30bc696e
muhammadgata <iamgata.me@gmail.com>
2023-09-06 21:22:12 +0700

    feat: setup poppins font using next/font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  preload: true,
+  display: "swap",
+  variable: "--font-poppins",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --font-poppins: ${poppins.style.fontFamily};
+        }
+        html {
+          font-family: var(--font-poppins);
+        }
+      `}</style>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
