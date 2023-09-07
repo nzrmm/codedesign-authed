7c47a82ef634171f7ed964dda2b3aea4d1b85e22
muhammadgata <iamgata.me@gmail.com>
2023-09-07 23:48:21 +0700

    feat: add head in login and register page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Link from "next/link";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
@@ -8,54 +9,61 @@ import { cn } from "@/utils/style";
 
 const Login: NextPageWithLayout = () => {
   return (
-    <div className={cn("flex flex-col justify-center items-center px-20")}>
-      <div className={cn("w-full text-center mb-10")}>
-        <p className={cn("text-4xl font-semibold text-neutral-900 mb-4")}>
-          Login
-        </p>
-        <p
-          className={cn(
-            "mx-auto text-sm text-neutral-500 leading-relaxed w-10/12"
-          )}
-        >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
-          harum natus numquam.
-        </p>
-      </div>
+    <>
+      <Head>
+        <title>Login</title>
+        <meta name="description" content="a login page for user" />
+      </Head>
 
-      <div className={cn("w-full flex flex-col gap-4 mb-8")}>
-        <TextInput type="email" placeholder="Email" />
-        <TextInput type="password" placeholder="Password" />
-      </div>
+      <div className={cn("flex flex-col justify-center items-center px-20")}>
+        <div className={cn("w-full text-center mb-10")}>
+          <p className={cn("text-4xl font-semibold text-neutral-900 mb-4")}>
+            Login
+          </p>
+          <p
+            className={cn(
+              "mx-auto text-sm text-neutral-500 leading-relaxed w-10/12"
+            )}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+            harum natus numquam.
+          </p>
+        </div>
 
-      <div className={cn("w-full mb-4")}>
-        <Button>Login</Button>
-      </div>
+        <div className={cn("w-full flex flex-col gap-4 mb-8")}>
+          <TextInput type="email" placeholder="Email" />
+          <TextInput type="password" placeholder="Password" />
+        </div>
 
-      <div className={cn("mb-4")}>
-        <p className={cn("text-sm text-neutral-500")}>-- Or --</p>
-      </div>
+        <div className={cn("w-full mb-4")}>
+          <Button>Login</Button>
+        </div>
 
-      <div className={cn("w-full flex flex-col gap-2 mb-10")}>
-        <Button variant={"outline-secondary"} className={cn("text-sm")}>
-          <FcGoogle size={20} />
-          Sign in with Google
-        </Button>
-        <Button variant={"outline-secondary"} className={cn("text-sm")}>
-          <FaGithub size={20} />
-          Sign in with Github
-        </Button>
-      </div>
+        <div className={cn("mb-4")}>
+          <p className={cn("text-sm text-neutral-500")}>-- Or --</p>
+        </div>
 
-      <div>
-        <p className={cn("text-sm text-neutral-500")}>
-          Don&apos;t have an account yet ?{" "}
-          <Link href={"/register"} className={cn("text-blue-600 underline")}>
-            Register
-          </Link>
-        </p>
+        <div className={cn("w-full flex flex-col gap-2 mb-10")}>
+          <Button variant={"outline-secondary"} className={cn("text-sm")}>
+            <FcGoogle size={20} />
+            Sign in with Google
+          </Button>
+          <Button variant={"outline-secondary"} className={cn("text-sm")}>
+            <FaGithub size={20} />
+            Sign in with Github
+          </Button>
+        </div>
+
+        <div>
+          <p className={cn("text-sm text-neutral-500")}>
+            Don&apos;t have an account yet ?{" "}
+            <Link href={"/register"} className={cn("text-blue-600 underline")}>
+              Register
+            </Link>
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Link from "next/link";
 import { NextPageWithLayout } from "../_app";
 
@@ -6,41 +7,48 @@ import { cn } from "@/utils/style";
 
 const Register: NextPageWithLayout = () => {
   return (
-    <div className={cn("flex flex-col justify-center items-center px-20")}>
-      <div className={cn("w-full text-center mb-10")}>
-        <p className={cn("text-4xl font-semibold text-neutral-900 mb-4")}>
-          Register
-        </p>
-        <p
-          className={cn(
-            "mx-auto text-sm text-neutral-500 leading-relaxed w-10/12"
-          )}
-        >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
-          harum natus numquam.
-        </p>
-      </div>
+    <>
+      <Head>
+        <title>Register</title>
+        <meta name="description" content="a register page for user" />
+      </Head>
 
-      <div className={cn("w-full flex flex-col gap-4 mb-8")}>
-        <TextInput type="text" placeholder="Username" />
-        <TextInput type="email" placeholder="Email" />
-        <TextInput type="password" placeholder="Password" />
-        <TextInput type="password" placeholder="Confirm Password" />
-      </div>
+      <div className={cn("flex flex-col justify-center items-center px-20")}>
+        <div className={cn("w-full text-center mb-10")}>
+          <p className={cn("text-4xl font-semibold text-neutral-900 mb-4")}>
+            Register
+          </p>
+          <p
+            className={cn(
+              "mx-auto text-sm text-neutral-500 leading-relaxed w-10/12"
+            )}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+            harum natus numquam.
+          </p>
+        </div>
 
-      <div className={cn("w-full mb-10")}>
-        <Button>Login</Button>
-      </div>
+        <div className={cn("w-full flex flex-col gap-4 mb-8")}>
+          <TextInput type="text" placeholder="Username" />
+          <TextInput type="email" placeholder="Email" />
+          <TextInput type="password" placeholder="Password" />
+          <TextInput type="password" placeholder="Confirm Password" />
+        </div>
 
-      <div>
-        <p className={cn("text-sm text-neutral-500")}>
-          Have an account ?{" "}
-          <Link href={"/login"} className={cn("text-blue-600 underline")}>
-            Login
-          </Link>
-        </p>
+        <div className={cn("w-full mb-10")}>
+          <Button>Login</Button>
+        </div>
+
+        <div>
+          <p className={cn("text-sm text-neutral-500")}>
+            Have an account ?{" "}
+            <Link href={"/login"} className={cn("text-blue-600 underline")}>
+              Login
+            </Link>
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
