6cca66198b60b5d5a837ceee7134d788428687b2
muhammadgata <iamgata.me@gmail.com>
2023-09-10 23:18:18 +0700

    feat: handle register user

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,11 +1,39 @@
+import { useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
-import { NextPageWithLayout } from "../_app";
+import { useRouter } from "next/router";
+import { NextPageWithLayout } from "@/pages/_app";
 
 import { Button, TextInput } from "@/components";
 import { cn } from "@/utils/style";
 
 const Register: NextPageWithLayout = () => {
+  const router = useRouter();
+  const [user, setUser] = useState(
+    {} as {
+      username: string;
+      email: string;
+      password: string;
+      confirmPassword: string;
+    }
+  );
+
+  const handleRegister = async () => {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    };
+
+    await fetch("http://localhost:3000/api/auth/register", options)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data) router.push("http://localhost:3000/login");
+      });
+  };
+
   return (
     <>
       <Head>
@@ -29,14 +57,42 @@ const Register: NextPageWithLayout = () => {
         </div>
 
         <div className={cn("w-full flex flex-col gap-4 mb-8")}>
-          <TextInput type="text" placeholder="Username" />
-          <TextInput type="email" placeholder="Email" />
-          <TextInput type="password" placeholder="Password" />
-          <TextInput type="password" placeholder="Confirm Password" />
+          <TextInput
+            type="text"
+            placeholder="Username"
+            value={user.username || ""}
+            onChange={({ target }) => {
+              setUser({ ...user, username: target.value });
+            }}
+          />
+          <TextInput
+            type="email"
+            placeholder="Email"
+            value={user.email || ""}
+            onChange={({ target }) => {
+              setUser({ ...user, email: target.value });
+            }}
+          />
+          <TextInput
+            type="password"
+            placeholder="Password"
+            value={user.password || ""}
+            onChange={({ target }) => {
+              setUser({ ...user, password: target.value });
+            }}
+          />
+          <TextInput
+            type="password"
+            placeholder="Confirm Password"
+            value={user.confirmPassword || ""}
+            onChange={({ target }) => {
+              setUser({ ...user, confirmPassword: target.value });
+            }}
+          />
         </div>
 
         <div className={cn("w-full mb-10")}>
-          <Button>Login</Button>
+          <Button onClick={handleRegister}>Login</Button>
         </div>
 
         <div>
