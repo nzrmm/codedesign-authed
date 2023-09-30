36b4cc4d4ac2d4bcb8a6312f525c219144e84d25
muhammadgata <iamgata.me@gmail.com>
2023-10-01 01:33:07 +0700

    refactor(layout): re-style login-register-layout

--- src/layouts/login-register-layout/index.tsx
+++ src/layouts/login-register-layout/index.tsx
@@ -7,24 +7,7 @@ type ILoginRegisterLayoutProps = {
 
 const LoginRegisterLayout = ({ children }: ILoginRegisterLayoutProps) => {
   return (
-    <div
-      className={cn(
-        "flex justify-center items-center h-screen",
-        "bg-gradient-to-br from-blue-400 to-blue-500"
-      )}
-    >
-      <div
-        className={cn(
-          "w-8/12 h-3/4 grid lg:grid-cols-2",
-          "bg-white rounded-md overflow-hidden"
-        )}
-      >
-        <div
-          className={cn("bg-gradient-to-br from-blue-500 to-purple-500")}
-        ></div>
-        {children}
-      </div>
-    </div>
+    <div className={cn("antialiased w-full min-h-screen")}>{children}</div>
   );
 };
 
