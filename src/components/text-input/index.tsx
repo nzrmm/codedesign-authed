01826a5e6c9b627af1ae01270db35ddadd08416d
muhammadgata <iamgata.me@gmail.com>
2023-10-01 01:32:14 +0700

    feat(component): update size and variant text-input component

--- src/components/text-input/index.tsx
+++ src/components/text-input/index.tsx
@@ -1,23 +1,26 @@
 import { ComponentProps } from "react";
 import { cn } from "@/utils/style";
 import { cva, type VariantProps } from "class-variance-authority";
-import { FieldErrors, UseFormRegister } from "react-hook-form";
+import { FieldErrors, FieldError, UseFormRegister } from "react-hook-form";
 
-const textInputVariants = cva(
-  "w-full rounded-lg focus:outline-none px-4 py-3 border",
-  {
-    variants: {
-      variant: {
-        outline: ["border-neutral-200 focus:ring-1 focus:ring-neutral-300"],
-      },
+const textInputVariants = cva("w-full rounded-lg focus:outline-none", {
+  variants: {
+    size: {
+      md: "h-16 px-10",
     },
-    defaultVariants: {
-      variant: "outline",
+    variant: {
+      primary: ["bg-[#F3F4F6]"],
     },
-  }
-);
+  },
+  defaultVariants: {
+    size: "md",
+    variant: "primary",
+  },
+});
 
 type ITextInputProps = {
+  id: string;
+  label?: string;
   wrapperClassName?: string;
   errors: FieldErrors;
   register: UseFormRegister<any>;
@@ -25,6 +28,8 @@ type ITextInputProps = {
   VariantProps<typeof textInputVariants>;
 
 const TextInput = ({
+  id,
+  label,
   errors,
   variant,
   register,
@@ -34,13 +39,26 @@ const TextInput = ({
 }: ITextInputProps) => {
   return (
     <div className={cn("w-full", wrapperClassName)}>
+      {label && (
+        <label htmlFor={id} className={cn("font-semibold")}>
+          {label}
+        </label>
+      )}
+
       <input
+        id={id}
         className={cn(textInputVariants({ variant, className }), {
-          "border-rose-500": errors[props.id!],
+          "mt-2": label,
         })}
-        {...register(props.id!, { required: props.required })}
+        {...(register && { ...register(id) })}
         {...props}
       />
+
+      {errors && errors[id] && (
+        <span className={cn("text-sm text-rose-500 block px-1 mt-2")}>
+          {(errors[id] as FieldError).message}
+        </span>
+      )}
     </div>
   );
 };
